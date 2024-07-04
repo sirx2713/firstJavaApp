95acc16e39b562e10d52c16468745ad677431fba
iSAD <bradx2713@gmail.com>
2024-07-04 20:01:13 +0200

    Add files via upload

---
+++ firstJavaApp.java
@@ -0,0 +1,16 @@
+ package AppOne;
+
+public class firstJavaApp {
+
+	public static void main(String[] args) {
+		System.out.println("© iSAD");
+		
+		var x = new firstJavaApp();
+		x.myCodes();
+
+	}
+	public void myCodes() {
+		System.out.println("Developing in Java");
+	}
+
+}
